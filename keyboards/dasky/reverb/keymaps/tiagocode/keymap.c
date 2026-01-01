781902098a2892bb6d4a952e860f52fe76cbc5dd
Santiago Marulanda <santiagomm1997@gmail.com>
2026-01-01 21:48:33 +0100

    Adjust reverb keymap like other ones

--- keyboards/dasky/reverb/keymaps/tiagocode/keymap.c
+++ keyboards/dasky/reverb/keymaps/tiagocode/keymap.c
@@ -15,13 +15,8 @@
  */
 #include QMK_KEYBOARD_H
 
-#if __has_include("keymap.h")
-#    include "keymap.h"
-#endif
-
 enum layers {
     _QWERTY,
-    _QWERTY_MACOS,
     _NAVIGATION,
     _NUMPAD,
     _SYMBOLS,
@@ -37,7 +32,6 @@ enum layers {
 #define ADJS MO(_ADJUST)
 #define NUM_TAB LT(_NUMPAD, KC_TAB)
 #define QUO_FUN LT(_FUNCTIONS, KC_QUOT)
-#define MAC_BSP LT(_QWERTY_MACOS, KC_BSPC)
 
 // Aliases for modifiers
 #define LCAPS SFT_T(KC_CAPS)
@@ -53,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * 1st layer -> QWERTY
      *
      * ,-----------------------------------------.                    ,-----------------------------------------.
-     * |      |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+     * |LCTRL |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | RCTRL|
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
      * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |MACBSP|
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -66,32 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                          `--------------------'            '------''-------------'
      */
     [_QWERTY] = LAYOUT(
-        XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
-         KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, MAC_BSP,
-        NUM_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, QUO_FUN,
-          LCAPS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   RCAPS,
-                                            KC_LCTL,     NAV,   KC_SPC,   KC_ENT,   SYMBS, KC_LALT
-    ),
-
-    /*
-     * 1.1 layer -> QWERTY (with command key for MacOS)
-     *
-     * ,-----------------------------------------.                    ,-----------------------------------------.
-     * |      |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |      |
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |NUMTAB|   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |   ;  |QUOFUN|
-     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * |LCAPS |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RCAPS |
-     * `-----------------------------------------/      /      \      \-----------------------------------------'
-     *                          |LCMD  | NAV  | /Space /        \Enter \  |SYMBS | LALT |
-     *                          |      |      |/      /          \      \ |      |      |
-     *                          `--------------------'            '------''-------------'
-     */
-    [_QWERTY_MACOS] = LAYOUT(
-        XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
-         KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
+        KC_LCTL,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_RCTL,
+         KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
         NUM_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, QUO_FUN,
           LCAPS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   RCAPS,
                                             KC_LCMD,     NAV,   KC_SPC,   KC_ENT,   SYMBS, KC_LALT
@@ -116,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAVIGATION] = LAYOUT(
         XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR,                     XXXXXXX, XXXXXXX,   KC_UP, XXXXXXX, XXXXXXX, _______,
-        _______, XXXXXXX, KC_LCTL, KC_LALT, KC_LSFT, XXXXXXX,                     KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,  KC_END,  KC_DEL,
-        _______, XXXXXXX, XXXXXXX, KC_LGUI,  KC_APP, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, KC_LCMD, KC_LCTL, KC_LALT, KC_LSFT, XXXXXXX,                     KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,  KC_END,  KC_DEL,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                                ADJS, XXXXXXX, _______,   _______,  KC_DEL, KC_BSPC
     ),
 
--- keyboards/omkbd/ergodash/keymaps/tiagocode/keymap.c
+++ keyboards/omkbd/ergodash/keymaps/tiagocode/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
     * | ESC  |   Q  |   W  |   E  |   R  |   T  |      |                    |      |   Y  |   U  |   I  |   O  |   P  |BACKSP|
     * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-    * |NUMTAB|   A  |   S  |   D  |   F  |   G  |      |                    |      |   H  |   J  |   K  |   L  |   ;  |QUOFUN|
+    * |NUMTAB|   A  |   S  |   D  |   F  |   G  |      |                    |      |   H  |   J  |   K  |   L  |   ;  | QUOT |
     * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
     * |LCAPS |   Z  |   X  |   C  |   V  |   B  |      |                    |      |   N  |   M  |   ,  |   .  |   /  | RCAPS|
     * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
         XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, XXXXXXX,                       XXXXXXX,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
          KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, XXXXXXX,                       XXXXXXX,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-        NUM_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, XXXXXXX,                       XXXXXXX,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, QUO_FUN,
+        NUM_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, XXXXXXX,                       XXXXXXX,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
           LCAPS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, XXXXXXX,                       XXXXXXX,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   RCAPS,
         XXXXXXX, XXXXXXX, KC_LCTL, KC_LCMD,              NAV,  KC_SPC,  KC_SPC,      KC_ENT,  KC_ENT,   SYMBS,            FUNCS, KC_RALT, XXXXXXX, XXXXXXX
     ),
